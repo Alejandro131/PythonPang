4f1843449aa83d57bc7b6e5ecebfe64fae386d4a
Alejandro131 <alex131_d@yahoo.com>
2013-05-27 19:16:23 +0300

    minor changes to menu

--- lib/menu/menu.py
+++ lib/menu/menu.py
@@ -5,14 +5,17 @@ from pang.lib.world.settings import *
 from pang.lib.world.vec2d import Vec2D
 
 class Menu:
-    def __init__(self, title):
+    def __init__(self, title=None, y_position=0):
         self.labels = OrderedDict()
         self.font_size = MENU_LABEL_FONT_SIZE
         self.font = pygame.font.SysFont(None, self.font_size)
         self.selected_color = (255, 0, 0)
         self.color = (255, 255, 255)
         self.selected_option = None
-        self.title_text = self.font.render(title, True, self.color)
+        self.title = title
+        self.y_position = y_position
+        if self.title:
+            self.title_text = self.font.render(title, True, self.color)
         self.dimmer_rect = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT),
                                           pygame.SRCALPHA, 32)
         self.dimmer_rect.fill((0, 0, 0, 192))
@@ -46,19 +49,24 @@ class Menu:
             
     def draw(self, screen):
         screen.blit(self.dimmer_rect, (0, 0))
-        max_height = self.title_text.get_rect().height
-        items = len(self.labels.items()) + 2
+        items = len(self.labels.items())
+        max_height = 0
+        if self.title:
+            max_height = self.title_text.get_rect().height
+            items += 2   
         for key, value in self.labels.items():
             text_height = value[0].get_rect().height
             if text_height > max_height:
                 max_height = text_height
         current_y_pos = (SCREEN_HEIGHT - (items * max_height + (items - 1) * \
                                           MENU_LABEL_MARGIN))//2
-        position = Vec2D((SCREEN_WIDTH - self.title_text.get_rect().width)//2, 
-                                     current_y_pos + (max_height - \
-                                                      self.title_text.get_rect().height)//2)
-        screen.blit(self.title_text, position)
-        current_y_pos += 2 * (max_height + MENU_LABEL_MARGIN)
+        current_y_pos += self.y_position
+        if self.title:
+            position = Vec2D((SCREEN_WIDTH - self.title_text.get_rect().width)//2, 
+                                         current_y_pos + (max_height - \
+                                                          self.title_text.get_rect().height)//2)
+            screen.blit(self.title_text, position)
+            current_y_pos += 2 * (max_height + MENU_LABEL_MARGIN)
         for key, value in self.labels.items():
             position = Vec2D((SCREEN_WIDTH - value[0].get_rect().width)//2, 
                              current_y_pos + (max_height - \
--- lib/score/scoreboard.py
+++ lib/score/scoreboard.py
@@ -2,5 +2,30 @@ import pygame
 from pygame.locals import *
 
 class ScoreBoard:
+    
     def __init__(self):
+        pass
+
+    def process_events(self):
+        pass
+    
+    def draw(self, screen):
+        pass
+    
+    def update(self, time_passed):
+        pass
+    
+    def check_score(self, score, level):
+        pass
+    
+    def clear_scores(self):
+        pass
+    
+    def load_scores(self):
+        pass
+    
+    def save_scores(self):
+        pass
+    
+    def add_score(self):
         pass
